83cf348acb57c3947104b8693f1c362b655443e0
LEOCHEUNG <leozhang621@gmail.com>
2024-12-03 01:38:13 +0800

    format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
-import { destroyGame, initGame } from './game.ts';
+import { Game } from './game.ts';
 import './App.css';
 
 const App = () => {
+  const game = useRef<Game>();
+
   useEffect(() => {
-    initGame();
-    return () => destroyGame();
+    game.current = new Game();
+    game.current.initGame();
+    return () => game.current?.destroyGame();
   }, []);
 
   return (
     <section className="w-screen h-screen flex justify-center items-center">
-      <div className="w-[800px] h-[800px] rounded-md overflow-hidden" id="game-container"></div>
+      <div
+        className="w-[800px] h-[800px] rounded-md overflow-hidden"
+        id="game-container"
+      />
     </section>
   );
 };
--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,22 @@
 import * as Phaser from 'phaser';
 
-let game: null | Phaser.Game = null;
+export class Game {
+  private game: Phaser.Game | null = null;
 
-export function initGame() {
-  game = new Phaser.Game({
-    parent: 'game-container',
-    scene: [MainScene],
-    type: Phaser.AUTO,
-    width: 800,
-    height: 800,
-    backgroundColor: '#fde047'
-  });
-}
+  initGame() {
+    this.game = new Phaser.Game({
+      parent: 'game-container',
+      scene: [MainScene],
+      type: Phaser.AUTO,
+      width: 800,
+      height: 800,
+      backgroundColor: '#fde047',
+    });
+  }
 
-export function destroyGame() {
-  game?.destroy(true);
+  destroyGame() {
+    this.game?.destroy(true);
+  }
 }
 
 class MainScene extends Phaser.Scene {
