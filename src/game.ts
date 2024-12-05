aa43f0424000615b937b0be1d9cf92a79f649bac
Leo Cheung <leozhang621@gmail.com>
2024-12-05 15:31:13 +0800

    refactor logic

--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,15 @@
 import * as Phaser from 'phaser';
 
+const { Vector2, Distance } = Phaser.Math;
+
+type TVector2 = Phaser.Math.Vector2;
+
 enum Direction {
   UP,
   DOWN,
   LEFT,
   RIGHT,
 }
-
-type Position = { x: number; y: number };
-type Body = Position & {
-  gameObject: Phaser.GameObjects.Arc | null;
-  pIndex: number;
-  direction: Direction;
-  midLine: Position[];
-};
 
 export class Game {
   private game: Phaser.Game | null = null;
@@ -36,112 +32,139 @@ export class Game {
 }
 
 class MainScene extends Phaser.Scene {
-  private body: Array<Body> = [];
-  private bodySize = 80;
   private direction: Direction = Direction.RIGHT;
-  private speed = 8;
-  private movePoints: Array<Pick<Body, 'x' | 'y' | 'direction' | 'midLine'>> = [];
-  private bodyPolygon1: Phaser.GameObjects.Graphics | null = null;
-  private bodyPolygon2: Phaser.GameObjects.Graphics | null = null;
+  private head: TVector2 = Vector2.ZERO;
+  private tail: TVector2 = Vector2.ZERO;
+  private breakPoints: Array<{ pos: TVector2; direction: Direction }> = [];
+  private headGraphics: Phaser.GameObjects.Arc | null = null;
+  private tailGraphics: Phaser.GameObjects.Arc | null = null;
+  private bodyGraphics: Phaser.GameObjects.Graphics | null = null;
+  private bodySize = 60;
+  private bodyCount = 8;
+  private baseSpeed = 10;
+  private rightSpeed: TVector2 = Vector2.ZERO;
+  private upSpeed: TVector2 = Vector2.ZERO;
+  private downSpeed: TVector2 = Vector2.ZERO;
+  private leftSpeed: TVector2 = Vector2.ZERO;
   // input lock
   private inputLock = false;
   private distanceAfterLastInput = 0;
+  // colors
+  private readonly primaryColor = 0x0ea5e9;
   // debug
-  private midLines: Array<Phaser.GameObjects.Graphics> = [];
-  private line: Phaser.GameObjects.Graphics | null = null;
-
-  constructor() {
-    super('MainScene');
-  }
+  private debugLine: Phaser.GameObjects.Graphics | null = null;
 
   init() {
-    this.body = [];
-    this.movePoints = [];
+    const { width, height } = this.scale.gameSize;
+    this.head = new Vector2(width / 2, height / 2);
+    this.tail = new Vector2(width / 2 - this.bodySize * this.bodyCount, height / 2);
+    this.rightSpeed = new Vector2(this.baseSpeed, 0);
+    this.upSpeed = new Vector2(0, -this.baseSpeed);
+    this.leftSpeed = new Vector2(-this.baseSpeed, 0);
+    this.downSpeed = new Vector2(0, this.baseSpeed);
   }
 
   create() {
-    const { width, height } = this.scale.gameSize;
-    // init body
-    const head: Body = {
-      x: width / 2,
-      y: height / 2,
-      gameObject: null,
-      pIndex: 0,
-      midLine: [
-        { x: 0, y: 0 },
-        { x: 0, y: 0 },
-      ],
-      direction: Direction.RIGHT,
-    };
-    const body = [head];
-    this.movePoints.push({
-      x: head.x,
-      y: head.y,
-      direction: Direction.RIGHT,
-      midLine: [
-        { x: 0, y: 0 },
-        { x: 0, y: 0 },
-      ],
-    });
-    Array(5)
-      .fill(1)
-      .forEach((_, index) => {
-        const prev = body[index];
-        const curr: Body = {
-          x: prev.x - this.bodySize,
-          y: prev.y,
-          gameObject: null,
-          pIndex: 0,
-          midLine: [
-            { x: 0, y: 0 },
-            { x: 0, y: 0 },
-          ],
-          direction: Direction.RIGHT,
-        };
-        body.push(curr);
-        let len = this.speed;
-        while (len <= this.bodySize) {
-          this.movePoints.push({
-            x: prev.x - len,
-            y: prev.y,
-            direction: Direction.RIGHT,
-            midLine: [
-              { x: 0, y: 0 },
-              { x: 0, y: 0 },
-            ],
-          });
-          len += this.speed;
-        }
-        curr.pIndex = this.movePoints.length - 1;
-      });
-    this.body = body;
-    this.body.forEach((b, index) => {
-      this.updateMidLine(b, b.pIndex);
-      if (index === 0 || index === this.body.length - 1) {
-        b.gameObject = this.add.circle(b.x, b.y, Phaser.Math.Distance.Between(b.midLine[0].x, b.midLine[0].y, b.midLine[1].x, b.midLine[1].y) / 2, 0x0ea5e9);
-      }
-    });
-    this.movePoints.forEach((p, index) => this.updateMidLine(p, index));
-    this.bodyPolygon1 = this.add.graphics({ fillStyle: { color: 0x0ea5e9 } });
-    this.bodyPolygon2 = this.add.graphics({ fillStyle: { color: 0x0ea5e9 } });
-    // this.updateBodyPolygon();
+    this.headGraphics = this.add.circle(this.head.x, this.head.y, this.bodySize / 2, this.primaryColor);
+    this.tailGraphics = this.add.circle(this.head.x, this.head.y, this.bodySize / 2, this.primaryColor);
+    this.bodyGraphics = this.add.graphics({ lineStyle: { width: this.bodySize, color: this.primaryColor } });
     // listen event
     this.input.keyboard?.on('keydown-UP', () => this.processCursorKey(Direction.UP));
     this.input.keyboard?.on('keydown-LEFT', () => this.processCursorKey(Direction.LEFT));
     this.input.keyboard?.on('keydown-DOWN', () => this.processCursorKey(Direction.DOWN));
     this.input.keyboard?.on('keydown-RIGHT', () => this.processCursorKey(Direction.RIGHT));
-    console.log(this.body);
-    console.log(this.movePoints);
   }
 
   update() {
-    this.moveSnake();
-    this.updateBodyPolygon();
+    this.moveHead();
+    this.breakPoints.length > 0 ? this.updateTailWithBreakPoints() : this.updateTail();
+    this.updateBody();
     this.processInputLock();
-    this.debug();
+    // this.debug();
   }
 
-  processCursorKey(direction: Direction) {
+  private moveHead() {
+    const { head, rightSpeed, leftSpeed, upSpeed, downSpeed, direction, bodySize, headGraphics } = this;
+    const { width, height } = this.scale.gameSize;
+    if (direction === Direction.RIGHT) head.add(rightSpeed!);
+    if (direction === Direction.LEFT) head.add(leftSpeed!);
+    if (direction === Direction.UP) head.add(upSpeed!);
+    if (direction === Direction.DOWN) head.add(downSpeed!);
+    if (head.x > width + bodySize / 2) head.x = 0;
+    if (head.x < -bodySize / 2) head.x = width;
+    if (head.y > height + bodySize / 2) head.y = 0;
+    if (head.y < -bodySize / 2) head.y = height;
+    if (headGraphics !== null) {
+      headGraphics.setPosition(head.x, head.y);
+    }
+  }
+
+  private updateTail() {
+    const bodyLength = this.bodySize * this.bodyCount;
+    this.tail = this.head.clone();
+    this.positionTail(this.direction, bodyLength);
+    if (this.tailGraphics) {
+      this.tailGraphics.setPosition(this.tail.x, this.tail.y);
+    }
+  }
+
+  private updateTailWithBreakPoints() {
+    const p = this.breakPoints[this.breakPoints.length - 1];
+    const bodyLength = this.bodyCount * this.bodySize;
+    let d = Distance.Between(this.head.x, this.head.y, p.pos.x, p.pos.y);
+    if (d >= bodyLength) {
+      this.breakPoints = [];
+      this.updateTail();
+      return;
+    }
+    let index = this.breakPoints.length - 1;
+    while (index > 0) {
+      const curr = this.breakPoints[index];
+      const prev = this.breakPoints[index - 1];
+      const _d = Distance.Between(prev.pos.x, prev.pos.y, curr.pos.x, curr.pos.y);
+      if (d + _d >= bodyLength) {
+        this.breakPoints = this.breakPoints.slice(index);
+        break;
+      } else {
+        d += _d;
+        index--;
+      }
+    }
+    this.tail = this.breakPoints[0].pos.clone();
+    this.positionTail(this.breakPoints[0].direction, bodyLength - d);
+    if (this.tailGraphics) {
+      this.tailGraphics.setPosition(this.tail.x, this.tail.y);
+    }
+  }
+
+  private updateBody() {
+    this.bodyGraphics?.clear().moveTo(this.head.x, this.head.y);
+    for (let i = this.breakPoints.length - 1; i >= 0; i--) {
+      const p = this.breakPoints[i];
+      this.bodyGraphics?.lineTo(p.pos.x, p.pos.y);
+    }
+    this.bodyGraphics?.lineTo(this.tail.x, this.tail.y);
+    this.bodyGraphics?.strokePath();
+  }
+
+  private positionTail(direction: Direction, bodyLength: number) {
+    switch (direction) {
+      case Direction.UP:
+        this.tail.add(this.getMoveVector(Vector2.DOWN.clone(), bodyLength));
+        break;
+      case Direction.DOWN:
+        this.tail.add(this.getMoveVector(Vector2.UP.clone(), bodyLength));
+        break;
+      case Direction.LEFT:
+        this.tail.add(this.getMoveVector(Vector2.RIGHT.clone(), bodyLength));
+        break;
+      case Direction.RIGHT:
+        this.tail.add(this.getMoveVector(Vector2.LEFT.clone(), bodyLength));
+        break;
+    }
+  }
+
+  private processCursorKey(direction: Direction) {
     if (this.inputLock) return;
     if (
       (direction === Direction.UP && this.direction !== Direction.DOWN) ||
@@ -149,14 +172,15 @@ class MainScene extends Phaser.Scene {
       (direction === Direction.LEFT && this.direction !== Direction.RIGHT) ||
       (direction === Direction.RIGHT && this.direction !== Direction.LEFT)
     ) {
+      this.breakPoints.push({ pos: this.head.clone(), direction: this.direction });
       this.direction = direction;
       this.inputLock = true;
     }
   }
 
-  processInputLock() {
+  private processInputLock() {
     if (this.inputLock) {
-      this.distanceAfterLastInput += this.speed;
+      this.distanceAfterLastInput += this.baseSpeed;
       if (this.distanceAfterLastInput >= this.bodySize) {
         this.distanceAfterLastInput = 0;
         this.inputLock = false;
@@ -164,175 +188,20 @@ class MainScene extends Phaser.Scene {
     }
   }
 
-  moveSnake() {
-    // move head
-    this.moveHead(this.body[0]);
-    this.updateMidLine(this.body[0], 0);
-    // move body
-    for (let i = 1; i < this.body.length; i++) {
-      const body = this.body[i];
-      const nextPosition = this.movePoints[body.pIndex];
-      body.x = nextPosition.x;
-      body.y = nextPosition.y;
-      body.gameObject?.setPosition(body.x, body.y);
-      body.direction = nextPosition.direction;
-      this.updateMidLine(body, body.pIndex);
-      if (i === this.body.length - 1) {
-        this.movePoints.pop();
-      }
+  private getMoveVector(direction: TVector2, distance: number) {
+    return direction.multiply(new Vector2(distance, distance));
+  }
+
+  private debug() {
+    if (this.debugLine === null) {
+      this.debugLine = this.add.graphics({ lineStyle: { width: 2, color: 0xff0000 } });
     }
-  }
-
-  updateMidLine(body: Pick<Body, 'midLine' | 'direction' | 'x' | 'y'>, index: number) {
-    const vl = this.movePoints.length * 4 * this.bodySize;
-    const angle = Math.atan2(this.bodySize / 2, vl);
-    const t = (vl - index * (this.bodySize / 2)) * Math.tan(angle);
-    if (body.direction === Direction.RIGHT) {
-      body.midLine[0].x = body.midLine[1].x = body.x;
-      body.midLine[0].y = body.y - t;
-      body.midLine[1].y = body.y + t;
+    this.debugLine.clear().moveTo(this.head.x, this.head.y);
+    for (let i = this.breakPoints.length - 1; i >= 0; i--) {
+      const p = this.breakPoints[i];
+      this.debugLine.lineTo(p.pos.x, p.pos.y);
     }
-    if (body.direction === Direction.LEFT) {
-      body.midLine[0].x = body.midLine[1].x = body.x;
-      body.midLine[0].y = body.y + t;
-      body.midLine[1].y = body.y - t;
-    }
-    if (body.direction === Direction.UP) {
-      body.midLine[0].y = body.midLine[1].y = body.y;
-      body.midLine[0].x = body.x - t;
-      body.midLine[1].x = body.x + t;
-    }
-    if (body.direction === Direction.DOWN) {
-      body.midLine[0].y = body.midLine[1].y = body.y;
-      body.midLine[0].x = body.x + t;
-      body.midLine[1].x = body.x - t;
-    }
+    this.debugLine.lineTo(this.tail.x, this.tail.y);
+    this.debugLine.strokePath();
   }
-
-  updateBodyPolygon() {
-    if (this.bodyPolygon1 === null) return;
-    if (this.bodyPolygon2 === null) return;
-    // this.updateMidLine(this.movePoints[0], 0);
-    // this.bodyPolygon1.clear();
-    // this.bodyPolygon2.clear();
-    // this.bodyPolygon1.beginPath();
-    // this.bodyPolygon2.beginPath();
-    // this.bodyPolygon1.moveTo(this.movePoints[0].midLine[0].x, this.movePoints[0].midLine[0].y);
-    // this.bodyPolygon2.moveTo(this.movePoints[0].midLine[1].x, this.movePoints[0].midLine[1].y);
-    this.movePoints.forEach((p, index) => {
-      // if (index === 0) return;
-      this.updateMidLine(p, index);
-      // this.bodyPolygon1?.lineTo(p.midLine[0].x, p.midLine[0].y);
-      // this.bodyPolygon2?.lineTo(p.midLine[1].x, p.midLine[1].y);
-    });
-    // this.bodyPolygon1.strokePath();
-    // this.bodyPolygon2.strokePath();
-  }
-
-  moveHead(body: Body) {
-    const { width, height } = this.scale.gameSize;
-    if (this.direction === Direction.RIGHT) body.x += this.speed;
-    if (this.direction === Direction.LEFT) body.x -= this.speed;
-    if (this.direction === Direction.UP) body.y -= this.speed;
-    if (this.direction === Direction.DOWN) body.y += this.speed;
-    if (body.x > width + this.bodySize / 2) body.x = 0;
-    if (body.x < -this.bodySize / 2) body.x = width;
-    if (body.y > height + this.bodySize / 2) body.y = 0;
-    if (body.y < -this.bodySize / 2) body.y = height;
-    body.gameObject?.setPosition(body.x, body.y);
-    body.direction = this.direction;
-    const newMovePoint = { x: body.x, y: body.y, direction: this.direction, midLine: this.getInitMidLine() };
-    this.movePoints.unshift(newMovePoint);
-  }
-
-  debug() {
-    if (this.midLines.length <= 0) {
-      this.movePoints.forEach(() => this.midLines.push(this.add.graphics({ lineStyle: { color: 0x0ea5e9, width: this.speed } })));
-    }
-    if (this.line === null) {
-      this.line = this.add.graphics({ lineStyle: { color: 0xff0000, width: 2 } });
-    }
-    this.midLines.forEach((g, index) => {
-      const { midLine } = this.movePoints[index];
-      g.clear();
-      g.beginPath();
-      g.moveTo(midLine[0].x, midLine[0].y);
-      g.lineTo(midLine[1].x, midLine[1].y);
-      g.closePath();
-      g.strokePath();
-    });
-    // this.line.clear();
-    // this.body.forEach((b, index) => {
-    //   if (index === 0) {
-    //     this.line?.moveTo(b.x, b.y);
-    //   } else {
-    //     this.line?.lineTo(b.x, b.y);
-    //   }
-    // });
-    // this.line.strokePath();
-  }
-
-  getDirection(p1: Position, p2: Position) {
-    if (p2.x === p1.x && p2.y === p1.y) return null;
-    if (p2.x > p1.x) return Direction.RIGHT;
-    if (p2.x < p1.x) return Direction.LEFT;
-    if (p2.y > p1.y) return Direction.DOWN;
-    if (p2.y < p1.y) return Direction.UP;
-  }
-
-  getInitMidLine() {
-    return [
-      { x: 0, y: 0 },
-      { x: 0, y: 0 },
-    ];
-  }
-
-  testSmoothPath() {
-    const points = [
-      { x: 100, y: 100 },
-      { x: 200, y: 50 },
-      { x: 300, y: 100 },
-      { x: 400, y: 150 },
-      { x: 500, y: 200 },
-      { x: 300, y: 400 },
-    ];
-    // const smoothPoints = generateBezierPoints(points, 100);
-    let graphics = this.add.graphics({ lineStyle: { width: 2, color: 0xff0000 } });
-    let graphicsSmooth = this.add.graphics({ lineStyle: { width: 2, color: 0x0000ff } });
-
-    graphics.beginPath();
-    graphics.moveTo(points[0].x, points[0].y);
-    points.forEach((p) => graphics.lineTo(p.x, p.y));
-    graphics.strokePath();
-
-    graphicsSmooth.beginPath();
-    graphicsSmooth.moveTo(points[0].x, points[0].y);
-    points.forEach((p) => {});
-    graphicsSmooth.strokePath();
-  }
-}
-
-function generateBezierPoints(points: Position[], segments: number) {
-  let result = [];
-
-  for (let i = -1; i < points.length - 2; i++) {
-    let p0 = points[Math.max(i, 0)];
-    let p1 = points[i + 1];
-    let p2 = points[i + 2];
-    let p3 = points[Math.min(i + 3, points.length - 1)];
-
-    for (let t = 0; t <= segments; t++) {
-      let tNormalized = t / segments;
-      let t2 = tNormalized * tNormalized;
-      let t3 = t2 * tNormalized;
-
-      let x = 0.5 * (2 * p1.x + (-p0.x + p2.x) * tNormalized + (2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x) * t2 + (-p0.x + 3 * p1.x - 3 * p2.x + p3.x) * t3);
-
-      let y = 0.5 * (2 * p1.y + (-p0.y + p2.y) * tNormalized + (2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y) * t2 + (-p0.y + 3 * p1.y - 3 * p2.y + p3.y) * t3);
-
-      result.push({ x, y });
-    }
-  }
-
-  return result;
 }
